babc85ff6eb2aa10b72243d124d3c66adaba844c
LJW <golove010@gmail.com>
2022-09-01 16:46:49 +0900

    새로운 문어

--- ggr.c
+++ ggr.c
@@ -1,5 +1,5 @@
 int main(){
-	printf("그래그래/n");
-	printf("어라 이건 뭐지");
+	printf("그래그래........../n");
+	printf("어라 이건 뭐지........");
 	retuen 0
 }
--- main.c
+++ main.c
@@ -2,7 +2,8 @@ serch(int a);
 int mian(){
 	int a=0;
 	scanf("%d",&a);
-printf("%d hello world 나는 문어 꿈을 꾸는 문어",serch(a));
+printf("%d hello world 나는 문어 꿈을 꾸는 문어\n",serch(a));
+printf("그래 너문어 해라");
 return -1;
 }
 int serch(int a){
